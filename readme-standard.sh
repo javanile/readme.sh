ae6b8a212fa13c20433c70d7cb9a5d722729122a
Francesco Bianco <info.francescobianco@gmail.com>
2020-05-25 22:31:23 +0200

    Update readme-standard.sh

--- readme-standard.sh
+++ readme-standard.sh
@@ -2,12 +2,16 @@
 
 input=README.md
 
-## Usage
-has_usage_=$(grep -e "^## Usage$" ${input} | cut -d: -f1)
-if ! ; then
-  echo "A"
-else 
-  echo "B"
+has_section() {
+  grep -e "^${1}$" ${input} | cut -d: -f1
+}
+
+error() {
+  echo ">>> $1"
+}
+
+section=Usage
+usage=$(has_section "## ${section}")
+if [ -z "${usage}" ]; then
+  error "Missing ${section} section, added by defulat at end of ${input}, refactor could required."
 fi
-
-
